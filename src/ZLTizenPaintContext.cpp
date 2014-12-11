6bd3c8f290a09fe7a605b6e121e6ab77b116c32e
Alex <temper8@gmail.com>
2014-12-11 23:26:26 +0300

    Добавил заготовки для PaintContext

---
+++ src/ZLTizenPaintContext.cpp
@@ -0,0 +1,18 @@
+/*
+ * ZLTizenPaintContext.cpp
+ *
+ *  Created on: Dec 11, 2014
+ *      Author: Alex
+ */
+
+#include "ZLTizenPaintContext.h"
+
+ZLTizenPaintContext::ZLTizenPaintContext() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ZLTizenPaintContext::~ZLTizenPaintContext() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/ZLTizenPaintContext.h
@@ -0,0 +1,17 @@
+/*
+ * ZLTizenPaintContext.h
+ *
+ *  Created on: Dec 11, 2014
+ *      Author: Alex
+ */
+
+#ifndef ZLTIZENPAINTCONTEXT_H_
+#define ZLTIZENPAINTCONTEXT_H_
+
+class ZLTizenPaintContext {
+public:
+	ZLTizenPaintContext();
+	virtual ~ZLTizenPaintContext();
+};
+
+#endif /* ZLTIZENPAINTCONTEXT_H_ */
