3e27dba515385a5312e7fe093441248fdddb8012
alex <temper8@gmail.com>
2015-02-12 03:33:30 +0300

    попытка добавить ресуры

--- zlibrary/ui/dialogs/ZLTizenOptionsDialog.cpp
+++ zlibrary/ui/dialogs/ZLTizenOptionsDialog.cpp
@@ -12,6 +12,8 @@
 #include "ZLTreeTitledNode.h"
 #include "../ZLTizenApplicationWindow.h"
 
+#define ELM_ZLUI_EDJ "/opt/usr/apps/org.tizen.tizen-fbreader/shared/res/ui.edj"
+
 ZLTizenOptionsDialog::~ZLTizenOptionsDialog() {
 	// TODO Auto-generated destructor stub
 }
@@ -42,37 +44,59 @@ static Evas_Object* create_scroller(Evas_Object *parent)
 
 	return scroller;
 }
+
+static Evas_Object*
+create_labels(Evas_Object *parent)
+{
+	Evas_Object *layout, *label, *label2, *label3, *label4, *label5;
+
+	layout = elm_layout_add(parent);
+	if (elm_layout_file_set(layout, ELM_ZLUI_EDJ, "label_layout") == 0) DBG("error set layout ");
+	evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+
+	label = elm_label_add(layout);
+	elm_object_part_content_set(layout, "label1", label);
+	elm_object_text_set(label, _("<align=left>left aligned label</align>"));
+
+	label2 = elm_label_add(layout);
+	elm_object_part_content_set(layout, "label2", label2);
+	elm_object_text_set(label2, _("<align=center>center aligned label<align>"));
+
+	label3 = elm_label_add(layout);
+	elm_object_part_content_set(layout, "label3", label3);
+	elm_label_wrap_width_set(label3, 100);
+	elm_object_text_set(label3, _("<align=right>right aligned label</align>"));
+
+   label4 = elm_label_add(layout);
+	elm_object_part_content_set(layout, "label4", label4);
+	elm_label_wrap_width_set(label4, 100);
+	elm_object_text_set(label4, _("<align=left>left aligned label</align>"));
+
+	label5 = elm_label_add(layout);
+	elm_object_part_content_set(layout, "label5", label5);
+	//elm_label_line_wrap_set(label5, EINA_TRUE);
+	elm_label_wrap_width_set(label5, 100);
+	elm_object_text_set(label5, _("<align=left>label changes line automatically if label wrap width is set and the text length is bigger than the width of the label</align>"));
+
+	return layout;
+}
+
 void ZLTizenOptionsDialog::createOptionsDialogObject(Evas_Object *nf){
 	DBG("createOptionsDialogObject");
-	Evas_Object *scroller, *layout;
-
-	scroller = create_scroller(nf);
+	Evas_Object *scroller, *layout, *label;
+//	scroller = create_scroller(nf);
+	//elm_naviframe_item_push(nf, "Linebreakmodes", NULL, NULL, scroller, NULL);
+//	elm_naviframe_item_push(nf, "Normal Styles", NULL, NULL, scroller, NULL);
 	//layout = create_button_view(scroller);
 
-	Evas_Object *btn, *img, *box;
+	scroller = create_scroller(nf);
+	elm_naviframe_item_push(nf, "Linebreakmodes", NULL, NULL, scroller, NULL);
 
-	box = elm_box_add(scroller);
-	evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	evas_object_size_hint_align_set(box, EVAS_HINT_FILL, EVAS_HINT_FILL);
-	elm_box_padding_set(box, 0, 5 * elm_config_scale_get());
-	evas_object_show(box);
+	layout = create_labels(nf);
 
-	/* icon_reorder style */
-	btn = elm_button_add(box);
-	elm_object_style_set(btn, "icon_reorder");
-	//evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)1);
-	evas_object_show(btn);
-	elm_box_pack_end(box, btn);
-	/* icon_expand_add style */
-	btn = elm_button_add(box);
-	elm_object_style_set(btn, "icon_expand_add");
-	//evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)2);
-	evas_object_show(btn);
-	elm_box_pack_end(box, btn);
+	elm_object_content_set(scroller, layout);
 
-	elm_object_content_set(scroller, box);
 
-	elm_naviframe_item_push(nf, "Normal Styles", NULL, NULL, scroller, NULL);
 }
 
 ZLDialogContent &ZLTizenOptionsDialog::createTab(const ZLResourceKey &key){
