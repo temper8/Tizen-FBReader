b28d5bca77d7f40524606840283fa0a2ec49cb0e
Alex <temper8@gmail.com>
2016-06-10 15:41:49 +0300

    добавил код для popup, но он не работает ((

--- zlibrary/ui/dialogs/optionView/TizenComboOptionView.cpp
+++ zlibrary/ui/dialogs/optionView/TizenComboOptionView.cpp
@@ -8,6 +8,7 @@
 
 #include "TizenComboOptionView.h"
 #include "../ZLTizenOptionsDialog.h"
+#include "../../ZLTizenApplicationWindow.h"
 
 #include "logger.h"
 
@@ -28,6 +29,74 @@ static void app_get_resource(const char *edj_file_in, char *edj_path_out, int ed
 	}
 }
 
+static char *items[] = {
+	"Text 1line", "Text 2line", "Text 3line",
+	"Text 4line", "Text 5line", "Text 6line",
+	"Text 7line", "Text 8line"
+};
+
+static char*
+gl_text_get_cb(void *data, Evas_Object *obj, const char *part)
+{
+	int index = (int) data;
+	return strdup(items[index]);
+}
+
+
+static void
+gl_sel_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	Evas_Object *popup = (Evas_Object *)data;
+	int index = 0;
+	Elm_Object_Item *item = (Elm_Object_Item *)event_info;
+
+	index = (int)elm_object_item_data_get(item);
+	printf("selected text %s\n",items[index]);
+	printf("Index of the list=%d\n", index);
+	evas_object_del(popup);
+}
+
+static void
+list_it_list_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	static Elm_Genlist_Item_Class itc;
+	Evas_Object *popup;
+	Evas_Object *box;
+	Evas_Object *genlist;
+	int i;
+	Evas_Object *win = (Evas_Object *)data;
+
+	popup = elm_popup_add(win);
+	elm_popup_align_set(popup, ELM_NOTIFY_ALIGN_FILL, 1.0);
+	eext_object_event_callback_add(popup, EEXT_CALLBACK_BACK, eext_popup_back_cb, NULL);
+	elm_object_part_text_set(popup, "title,text", "Title");
+	evas_object_size_hint_weight_set(popup, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+
+	/* box */
+	box = elm_box_add(popup);
+	evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+
+	/* genlist */
+	genlist = elm_genlist_add(box);
+	evas_object_size_hint_weight_set(genlist, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	evas_object_size_hint_align_set(genlist, EVAS_HINT_FILL, EVAS_HINT_FILL);
+
+	itc.item_style = "default";
+	itc.func.text_get = gl_text_get_cb;
+	itc.func.content_get = NULL;
+	itc.func.state_get = NULL;
+	itc.func.del = NULL;
+
+	for (i = 0; i < 8; i++) {
+		elm_genlist_item_append(genlist, &itc, (void *) i, NULL, ELM_GENLIST_ITEM_NONE, gl_sel_cb, popup);
+	}
+	evas_object_show(genlist);
+	elm_box_pack_end(box, genlist);
+	evas_object_size_hint_min_set(box, -1, 192);
+	elm_object_content_set(popup, box);
+
+	evas_object_show(popup);
+}
 
 //#define EDJ_FILE "edje/text.edj"
 #define EDJ_FILE "edje/combo.edj"
@@ -63,6 +132,9 @@ Evas_Object* TizenComboOptionView::createViewItem(Evas_Object *parent){
 	text = "<font_size=30>" + text + "</font_size>";
 	elm_object_text_set(label,_(text.c_str()));
 	elm_object_part_content_set(layout, "label", label);
+	ZLTizenApplicationWindow *tw =  myTab->myTizenOptionsDialog->myWindows;
+	evas_object_smart_callback_add(layout, "clicked", list_it_list_cb,tw->win);
+
 
 /*	check = elm_check_add(layout);
 	//elm_object_text_set(check, "Default");
